4717f102576dc47043da717c6f1fbe55a2259473
pratik-jadhav23 <pratik.jadhav@mmit.edu.in>
2025-03-22 00:19:10 +0530

    msm

--- video116_useContext_inReact/src/App.jsx
+++ video116_useContext_inReact/src/App.jsx
@@ -8,19 +8,13 @@ import { createContext } from "react";
 
 function App() {
   const [count, setCount] = useState(0)
-  const counterContext = createContext(0)
+  // const counterContext = createContext(0)
 
   return (
     <>
       <counterContext.Provider value={{count, setCount}}>
         <Navbar />
         <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
         </div>
         <h1>Vite + React</h1>
         <div className="card">
--- video116_useContext_inReact/src/components/Button.jsx
+++ video116_useContext_inReact/src/components/Button.jsx
@@ -4,9 +4,10 @@ import { counterContext } from '../context/context'
 
 const Button = () => {
     const value = useContext(counterContext)
+    console.log(value)
   return (
     <div>
-      <button onClick={() => value.setCount((count) => count + 1)}><span><Component2/></span> This is button</button>
+      <button onClick={() => value.setCount((count) => count + 1)}><span><Component2/></span> This is button click here</button>
     </div>
   )
 }
